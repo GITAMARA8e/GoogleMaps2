233613f7e7aa9313aa0afd8dfc42f8c303dc0852
unknown <sistemas@CHI051PCAL009.alumnos.cst>
2023-10-02 16:17:09 -0300

    Initial commit

---
+++ app/src/main/java/cl/ejemplo/googlemaps/MainActivity.java
@@ -0,0 +1,36 @@
+package cl.ejemplo.googlemaps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment =(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap=googleMap;
+        LatLng Sydney= new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(Sydney).title("Marcador Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Sydney));
+
+    }
+}
